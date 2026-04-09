078bebbbcb5e8f99e28b0a9d737b086f3b5248a1
sshuklaamberr <shuklaamber01@gmail.com>
2026-04-09 10:24:25 +0530

    UC16: Sort passenger bogies

---
+++ UC16.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class UC16 {
+    public static void main(String[] args) {
+        List<Integer> l = Arrays.asList(30,10);
+        Collections.sort(l);
+        System.out.println(l);
+    }
+}
