a592c7edca560cc2fe5fd3f94f47e05d15fcc2d3
sshuklaamberr <shuklaamber01@gmail.com>
2026-04-09 09:48:44 +0530

    UC5: Preserve insertion order using LinkedHashSet

---
+++ UC5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class UC5 {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+        set.add("B1");
+        set.add("B2");
+        System.out.println(set);
+    }
+}
